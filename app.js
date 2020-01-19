e48ea80264794ad856085e989e6904da9bf1b0ea
sunhaikuo <sunhaikuo@163.com>
2020-01-19 16:55:22 +0800

    * master feat: test2

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/api/detail', async (req, res) => {
-  console.log('Hello 21')
+  console.log('Hello 3')
 })
 
 app.get('/api/detail', async (req, res) => {
