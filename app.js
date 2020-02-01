0b1fbdfb439761be056e221a5fa6f4b2ad9fc72a
孙海阔 <sunhaikuo@sunhk-MacBook-Pro.local>
2020-02-01 10:50:12 +0800

    增加cors

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const bookmark = require('./bookmark')
 var bodyParser = require("body-parser")
 const { execSync } = require('child_process')
 var app = express();
+
+app.all('*', function(req, res, next) {  
+  res.header("Access-Control-Allow-Origin", "*");  
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");  
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
+  res.header("X-Powered-By",' 3.2.1')  
+  res.header("Content-Type", "application/json;charset=utf-8");  
+  next();  
+});  
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
