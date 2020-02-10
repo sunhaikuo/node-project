b6032b0aa50257ba2b8d82bafa534aa6fda2cbd2
sunhaikuo <sunhaikuo@meituan.com>
2020-02-10 18:33:09 +0800

    增加分页

--- app.js
+++ app.js
@@ -31,14 +31,14 @@ function getOption(url) {
   }
 }
 
-function getWeiboList(fid, containerid) {
+function getWeiboList(fid, containerid, max_id) {
   const url = 'https://api.weibo.cn/2/statuses/unread_hot_timeline?gsid=_2A25zDRKEDeRxGeBO41oT9SbLyD6IHXVRmyFMrDV6PUJbkdAKLWHEkWpNRbgUgipW6LAu9FJlfHoZrTc2Rxkz0dCG&sensors_mark=0&wm=3333_2001&sensors_is_first_day=true&from=109C393010&b=0&c=iphone&networktype=wifi&skin=default&v_p=80&v_f=1&s=7f247ef8&sensors_device_id=0A3D4E51-89B6-465D-9C31-2A1301EBD21D&lang=zh_CN&sflag=1&ua=iPhone8,1__weibo__9.12.3__iphone__os12.4&ft=0&aid=01AylOcmGTpYv4mLLh2Lb0ibHWogzZWpF5xMfSOxWrEckgXwo.&launchid=10000365--x'
   const param = {
     url,
     headers: {
       "content-type": "application/x-www-form-urlencoded;charset=utf-8",
     },
-    body: `extparam=discover%7Cnew_feed&fid=${fid}&uicode=10000495&count=25&trim_level=1&trim_page_recom=0&containerid=${containerid}&fromlog=102803&uid=6894452157&orifid=&refresh_sourceid=10000365&featurecode=10000001&preAdInterval=2&oriuicode=&daily_total_times=9&since_id=4413935291614270&need_jump_scheme=1`
+    body: `extparam=discover%7Cnew_feed&fid=${fid}&uicode=10000495&count=25&trim_level=1&trim_page_recom=0&containerid=${containerid}&fromlog=102803&uid=6894452157&orifid=&refresh_sourceid=10000365&featurecode=10000001&preAdInterval=2&oriuicode=&daily_total_times=9&since_id=4413935291614270&need_jump_scheme=1&${max_id == '0' ? '' : `max_id=${max_id}`}`
     // body: `extparam=discover%7Cnew_feed&fid=${fid}&containerid=${containerid}`
   }
   console.log(param)
@@ -83,7 +83,7 @@ function getChannel() {
 }
 
 app.get('/api', async (req, res) => {
-  const { fid, containerid } = req.query
+  const { fid, containerid, max_id } = req.query
   const body = await getWeiboList(fid, containerid)
   res.send(body)
 })
